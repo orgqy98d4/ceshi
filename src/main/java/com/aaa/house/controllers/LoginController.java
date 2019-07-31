e4064f23b4d27565ac6cfe0cb354bd0cee00d9f3
zongkeli-94 <691436977@qq.com>
2019-07-31 15:43:04 +0800

    2

--- src/main/java/com/aaa/house/controllers/LoginController.java
+++ src/main/java/com/aaa/house/controllers/LoginController.java
@@ -1,14 +1,17 @@
 package com.aaa.house.controllers;
 
+import com.aaa.house.service.EmployeeService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -19,6 +22,9 @@ import java.util.Map;
  */
 @RestController
 public class LoginController {
+
+    @Autowired
+    private EmployeeService employeeService;
 
     /**
      * 登录验证
@@ -31,29 +37,47 @@ public class LoginController {
         //获取登录账号及密码
         String eusername = map.get("eusername") + "";
         String epassword = map.get("epassword") + "";
+        //判断登录的账号是否可用
+        Map mapt=new HashMap();
+        mapt.put("eusername",eusername);
+        mapt.put("start",0);
+        mapt.put("pageSize",100);
+        List<Map> list = employeeService.queryAll(mapt);
         //构建Subject对象（当前用户）
         Subject subject = SecurityUtils.getSubject();
         String msg="";
-        if (eusername!=null&&!"".equals(eusername)) {
-            //构建认证的身份令牌
-            UsernamePasswordToken token = new UsernamePasswordToken(eusername, epassword);
+        if (list!=null&&list.size()>0){
+            Map map1 = list.get(0);
+            //获取登录用户的状态
+            Object o = map1.get("stateid");
+            System.out.println(o);
             try {
-                //将身份令牌放进去
-                subject.login(token);
-                msg="suc";
-            } catch (AuthenticationException exception) {
+                if (o.equals(4)){
+                    throw new UnknownAccountException("用户不存在");
+                }else if (eusername!=null&&!"".equals(eusername)) {
+                    //构建认证的身份令牌
+                    UsernamePasswordToken token = new UsernamePasswordToken(eusername, epassword);
+                    try {
+                        //将身份令牌放进去
+                        subject.login(token);
+                        msg="suc";
+                    } catch (AuthenticationException exception) {
 //                //清除session
-                token.clear();
-                //自定义信息
-                if (UnknownAccountException.class.getName().equals(exception.getClass().getName())){
-                    msg="您输入的账号不存在";
-                }else if(IncorrectCredentialsException.class.getName().equals(exception.getClass().getName())){
-                    msg="账号或密码不匹配";
-                }else if (LockedAccountException.class.getName().equals(exception.getClass().getName())){
-                    msg="您的账号已被锁定，无法登录系统";
-                }else {
-                    msg="账号或密码错误";
+                        token.clear();
+                        //自定义信息
+                        if (UnknownAccountException.class.getName().equals(exception.getClass().getName())){
+                            msg="您输入的账号不存在";
+                        }else if(IncorrectCredentialsException.class.getName().equals(exception.getClass().getName())){
+                            msg="账号或密码不匹配";
+                        }else if (LockedAccountException.class.getName().equals(exception.getClass().getName())){
+                            msg="您的账号已被锁定，无法登录系统";
+                        }else {
+                            msg="账号或密码错误";
+                        }
+                    }
                 }
+            } catch (UnknownAccountException e) {
+                  msg="该账号已被禁用";
             }
         }
         Map maptmp=new HashMap();
